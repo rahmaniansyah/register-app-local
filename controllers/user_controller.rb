112c004dce2d2492afb50ec53a30d0604af85ecf
Asep Hidayat <rahmaputri13@gmail.com>
2020-11-05 12:01:25 +0700

    give error handling when succed

--- controllers/user_controller.rb
+++ controllers/user_controller.rb
@@ -1,14 +1,6 @@
 require './models/user'
 
 class UserController
-    def initialize
-        # @phone_number = params["phone_number"]
-        # @first_name = params["first_name"]
-        # @last_name = params["last_name"]
-        # @dob = [params["year"], params["month"], params["date"]].join('-')
-        # @gender = params["gender"]
-        # @email = params["email"]
-    end
     
     def index
         renderer = ERB.new(File.read("./views/index.erb"))
@@ -17,25 +9,20 @@ class UserController
 
     def add_user(params)
        
-        # @user = User.new(phone_number: @phone_number, first_name: @first_name, 
-        #                 last_name: @last_name, dob: @dob, gender: @gender, email: @email)
-        dob = [params["year"], params["month"], params["date"]].join('-')
+        raw_dob = [params["year"], params["month"], params["date"]]
+        dob = raw_dob.join('-') if raw_dob.all?
         user = User.new(phone_number: params["phone_number"], first_name: params["first_name"], 
                         last_name: params["last_name"], dob: dob, gender: params["gender"], email: params["email"])
         
-        # @user.save!
         if user.valid?
             user.save!
-            # redirect "/people/#{@person.id}"
+            @user_valid = true
         else
             @messages = user.errors.messages
-            renderer = ERB.new(File.read("./views/index.erb"))
-            renderer.result(binding)
         end
+        
+        renderer = ERB.new(File.read("./views/index.erb"))
+        renderer.result(binding)
     
-        # user = User.new(params["phone_number"], params["first_name"], 
-        #                 params["last_name"], dob, params["gender"], params["email"])
-        # user.save
-
     end
 end
